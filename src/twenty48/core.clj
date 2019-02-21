902a6c12ebcb0ae5df10a8d4455ad48c3c34f0cc
Shubham <shubhamj7997@gmail.com>
2019-02-21 17:28:30 +0530

    added move grid up and down

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -3,28 +3,39 @@
 
 (def add-combinations (partial map (partial apply +)))
 
-(def combinations-to-add (comp (partial mapcat (partial partition-all 2)) (partial partition-by identity)))
+(def combinations-to-add (comp
+                          (partial mapcat (partial partition-all 2))
+                          (partial partition-by identity)))
 
-(def add-consecutive-equal-element (comp add-combinations combinations-to-add))
+(def add-consecutive-equal-element (comp
+                                    add-combinations
+                                    combinations-to-add))
 
-(def filter-zeros (partial filter (comp not (partial zero?))))
+(def filter-zeros (partial remove zero?))
 
 (defn fill-zeros-to-right [row] (take 4 (concat row (repeat 0))))
 
-(def move-left (comp fill-zeros-to-right add-consecutive-equal-element filter-zeros))
+(def turn-grid (partial apply map vector))
+
+(def move-left (comp
+                fill-zeros-to-right
+                add-consecutive-equal-element
+                filter-zeros))
 
 ; "Moves an entire grid to the right"
-(def move-grid-right (comp (partial map (comp reverse move-left reverse))))
+(def move-grid-right (partial map (comp reverse move-left reverse)))
 
 ; "Moves an entire grid to the left"
-(def move-grid-left (comp (partial map move-left)))
+(def move-grid-left (partial map move-left))
 
 ; "Moves an entire grid down"
-(defn move-grid-down
-  [grid]
-  grid)
+(def move-grid-down (comp
+                     turn-grid
+                     move-grid-right
+                     turn-grid))
 
 ; "Moves an entire grid up"
-(defn move-grid-up
-  [grid]
-  grid)
+(def move-grid-up (comp
+                   turn-grid
+                   move-grid-left
+                   turn-grid))
