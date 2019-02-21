bf5226a55b07ac8726962c3a93a2cc049c7b0b17
Shubham <shubhamj7997@gmail.com>
2019-02-21 16:41:16 +0530

    add move left and right

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,30 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
-  "Moves an entire grid to the right"
-  [grid]
-  grid)
+(def add-combinations (partial map (partial apply +)))
 
-(defn move-grid-left
-  "Moves an entire grid to the left"
-  [grid]
-  grid)
+(def combinations-to-add (comp (partial mapcat (partial partition-all 2)) (partial partition-by identity)))
 
+(def add-consecutive-equal-element (comp add-combinations combinations-to-add))
+
+(def filter-zeros (partial filter (comp not (partial zero?))))
+
+(defn fill-zeros-to-right [row] (take 4 (concat row (repeat 0))))
+
+(def move-left (comp fill-zeros-to-right add-consecutive-equal-element filter-zeros))
+
+; "Moves an entire grid to the right"
+(def move-grid-right (comp (partial map (comp reverse move-left reverse))))
+
+; "Moves an entire grid to the left"
+(def move-grid-left (comp (partial map move-left)))
+
+; "Moves an entire grid down"
 (defn move-grid-down
-  "Moves an entire grid down"
   [grid]
   grid)
 
+; "Moves an entire grid up"
 (defn move-grid-up
-  "Moves an entire grid up"
   [grid]
   grid)
